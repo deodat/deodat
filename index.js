41ff9f60f9be744585b4a3ea7e16fd00a6801a0f
David Gourdet <gourdet.david@gmail.com>
2022-12-10 12:40:59 +0100

    Add first version
    Signed-off-by: David Gourdet <gourdet.david@gmail.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+
+David Gourdet - Garnement
+Creative frontend dev • Freelancer • Ex Nouvelle Vague
+♥️  #GSAP #ThreeJS #WebGL #Shaders #Nuxt #Storyblok ♥️
+Golf addict • Co-creator coeurdegreen.fr
+
+📖 GitHub: https://github.com/deodat
+🖊️ Codepen: https://codepen.io/deodat
+
+`)
