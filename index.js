cebd17a292a6bad0016334be1f889091dba8a11d
David Gourdet <gourdet.david@gmail.com>
2022-12-31 17:11:26 +0100

    👌 IMPROVE: Info

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const dim = chalk.dim
 const codepenColor = chalk.hex('#0ebeff').bold.inverse
 const githubColor = chalk.hex('#6cc644').bold.inverse
 
+// Alerts
+const sym = require('log-symbols')
+
+const success = chalk.green
+const info = chalk.blue
+const warning = chalk.keyword('orange')
+const error = chalk.red.bold
+
+
 welcome({
   title: 'David Gourdet aka Deodat - Garnement',
   tagLine: `Howdy, nice to meet ya!`,
@@ -28,5 +37,8 @@ Golf addict • Co-creator coeurdegreen.fr`
 
 ${githubColor(' GitHub ')}  ${dim(`https://github.com/deodat`)}
 ${codepenColor(' Codepen ')} ${dim(`https://codepen.io/deodat`)}
+`)
 
+log(`
+${sym.info} ${info(` INFO `)} This is a CLI app!
 `)
