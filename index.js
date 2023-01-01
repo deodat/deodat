f1dcaaf39d287385542bfad8c4b3a0d5f2743d73
David Gourdet <gourdet.david@gmail.com>
2023-01-01 19:40:31 +0100

    📦 NEW: Refactor init

--- index.js
+++ index.js
@@ -1,47 +1,27 @@
 #!/usr/bin/env node
 
-const pkgJSON = require('./package.json')
-const welcome = require('cli-welcome');
 const chalk = require('chalk')
-const checkNode = require('cli-check-node')
+const alert = require('cli-alerts');
+const init = require('./utils/init')
 
-const log = console.log
 const italic = chalk.italic
 const dim = chalk.dim
-const codepenColor = chalk.hex('#0ebeff').bold.inverse
-const githubColor = chalk.hex('#6cc644').bold.inverse
+const codepenColor = chalk.hex('#0ebeff').bold.inverse;
+const githubColor = chalk.hex('#6cc644').bold.inverse;
 
-// Alerts
-const sym = require('log-symbols')
 
-const success = chalk.green
-const info = chalk.blue
-const warning = chalk.keyword('orange')
-const error = chalk.red.bold
+(async () => {
+  init();
 
-welcome({
-  title: 'David Gourdet aka Deodat - Garnement',
-  tagLine: `Howdy, nice to meet ya!`,
-  description: pkgJSON.description,
-  version: pkgJSON.version,
-  bgColor: `#FADC00`,
-  color: `#000000`,
-  bold: true,
-  clear: true
-})
-
-checkNode('14')
-
-log(`${italic(
-  `Creative frontend dev • Freelancer • Ex Nouvelle Vague
+  console.log(`${italic(
+`Creative frontend dev • Freelancer • Ex Nouvelle Vague
 ♥️  #GSAP #ThreeJS #WebGL #Shaders #Nuxt #Storyblok ♥️
 Golf addict • Co-creator coeurdegreen.fr`
-)}
+    )}
 
-${githubColor(' GitHub ')}  ${dim(`https://github.com/deodat`)}
-${codepenColor(' Codepen ')} ${dim(`https://codepen.io/deodat`)}
-`)
+    ${githubColor(' GitHub ')}  ${dim(`https://github.com/deodat`)}
+    ${codepenColor(' Codepen ')} ${dim(`https://codepen.io/deodat`)}
+  `);
 
-log(`
-${sym.info} ${info(` INFO `)} This is a CLI app!
-`)
+  alert({ type: 'info', msg: `Check out my NodeCLI.com course.` });
+})();
